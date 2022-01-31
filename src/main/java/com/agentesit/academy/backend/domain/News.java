43344b166e27ab10d327ceb2a0cb3c720e7d62e5
Kuba <kupca@seznam.cz>
2022-01-31 17:06:28 +0100

    Edit annotations in "News" class.

--- src/main/java/com/agentesit/academy/backend/domain/News.java
+++ src/main/java/com/agentesit/academy/backend/domain/News.java
@@ -2,6 +2,7 @@ package com.agentesit.academy.backend.domain;
 
 import com.sun.istack.NotNull;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -10,18 +11,16 @@ import java.time.LocalDateTime;
 @Entity
 public class News extends Record {
 
-    @NotBlank(message = "Title is mandatory!")
-    @Size(max = 200)
+    @Column(nullable = false, length = 200)
     private String title;
 
-    @NotBlank(message = "Perex is mandatory!")
-    @Size(max = 450)
+    @Column(nullable = false, length = 450)
     private String perex;
 
-    @NotBlank(message = "Content text is mandatory!")
+    @Column(nullable = false)
     private String contentText;
 
-    @NotNull
+    @Column(nullable = false)
     private CategoryOfNews category;
 
 
