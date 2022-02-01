6e3986c6d583fac6046318063f549a815b44be22
Kuba <kupca@seznam.cz>
2022-02-01 18:07:32 +0100

    Add instance variables, constructor, getters and setters to class "ImageEntity".

--- src/main/java/com/agentesit/academy/backend/domain/ImageEntity.java
+++ src/main/java/com/agentesit/academy/backend/domain/ImageEntity.java
@@ -1,4 +1,38 @@
 package com.agentesit.academy.backend.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity(name = "image")
 public class ImageEntity {
+
+    /** Relative path to image on the disk */
+    @Column(nullable  =  false)
+    private String pathToImage;
+
+    /** MIME type of file */
+    @Column(nullable  =  false)
+    private String mimeType;
+
+    // Constructor
+    public ImageEntity() {
+    }
+
+    // Getters and Setters
+
+    public String getPathToImage() {
+        return pathToImage;
+    }
+
+    public void setPathToImage(String pathToImage) {
+        this.pathToImage = pathToImage;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
 }
