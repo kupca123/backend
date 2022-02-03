46571e5b4ccf66f4258757d6d2a2f5762dfd20d2
Kuba <kupca@seznam.cz>
2022-02-03 17:26:42 +0100

    Add anotation to class "NewsService".

--- src/main/java/com/agentesit/academy/backend/service/NewsService.java
+++ src/main/java/com/agentesit/academy/backend/service/NewsService.java
@@ -1,7 +1,15 @@
 package com.agentesit.academy.backend.service;
 
+import com.agentesit.academy.backend.repository.NewsRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 /** Class managing data (News) between Controller and Repository. */
+@Service
 public class NewsService {
+
+@Autowired
+private NewsRepository newsRepository;
 
 
 
