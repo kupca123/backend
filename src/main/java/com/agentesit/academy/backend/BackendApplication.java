4bc8d757485ec1e4935978feeb25413ecd9b0397
Kuba <kupca@seznam.cz>
2022-02-03 19:15:08 +0100

    Add test initialize database data to "BackendApplication" - create "initializationDB" method.

--- src/main/java/com/agentesit/academy/backend/BackendApplication.java
+++ src/main/java/com/agentesit/academy/backend/BackendApplication.java
@@ -1,13 +1,25 @@
 package com.agentesit.academy.backend;
 
+import com.agentesit.academy.backend.domain.CategoryOfNews;
+import com.agentesit.academy.backend.domain.NewsEntity;
+import com.agentesit.academy.backend.repository.NewsRepository;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner initializationDB(NewsRepository newsRepository){
+		return args -> {
+			newsRepository.save(new NewsEntity("Nadpis", "Perex", "Obsah", CategoryOfNews.interní));
+		};
 	}
 
 }
