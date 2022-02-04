733c08eb799fdd04c3ae1e824aa7b3fce76f35f5
Kuba <kupca@seznam.cz>
2022-02-04 22:40:16 +0100

    Add new method "saveImage()" class "ImageService"

--- src/main/java/com/agentesit/academy/backend/service/ImageService.java
+++ src/main/java/com/agentesit/academy/backend/service/ImageService.java
@@ -1,5 +1,6 @@
 package com.agentesit.academy.backend.service;
 
+import com.agentesit.academy.backend.domain.ImageEntity;
 import com.agentesit.academy.backend.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,14 @@ public class ImageService {
 
     @Autowired
     private ImageRepository imageRepository;
+
+    /**
+     * Save (INSERT) new image to database.
+     * @param image Image to save.
+     * @return The image that was saved.
+     */
+    public ImageEntity saveImage(ImageEntity image){
+        return imageRepository.save(image);
+
+    }
 }
