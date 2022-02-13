b4b9d7867997079b7c05b36da5e9982413b5a6a7
Kuba <kupca@seznam.cz>
2022-02-13 12:11:59 +0100

    Add method getAllNews() to class NewsRestController.

--- src/main/java/com/agentesit/academy/backend/api/v1/news/NewsRestController.java
+++ src/main/java/com/agentesit/academy/backend/api/v1/news/NewsRestController.java
@@ -1,7 +1,13 @@
 package com.agentesit.academy.backend.api.v1.news;
 
+import com.agentesit.academy.backend.domain.NewsEntity;
+import com.agentesit.academy.backend.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Rest Controller class for News.
@@ -10,5 +16,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/news")
 public class NewsRestController {
 
+    /** Instance of NewsService class */
+    @Autowired
+    private NewsService newsService;
 
+    /**
+     * Get all news from database.
+     * @return Get List of news from database.
+     */
+    @GetMapping
+    public List<NewsEntity> getAllNews(){
+        return newsService.getAllNews();
+    }
 }
