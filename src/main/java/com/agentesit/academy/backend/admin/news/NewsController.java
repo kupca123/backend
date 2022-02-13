cbed0fdcd0c62dbdd86218df03847e2f7a6e4671
Kuba <kupca@seznam.cz>
2022-02-13 12:32:38 +0100

    Create new class NewsController.

---
+++ src/main/java/com/agentesit/academy/backend/admin/news/NewsController.java
@@ -0,0 +1,10 @@
+package com.agentesit.academy.backend.admin.news;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/news")
+public class NewsController {
+
+}
