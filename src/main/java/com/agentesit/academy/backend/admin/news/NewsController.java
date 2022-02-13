ffdc12f878f1ad454dd685cc33a4d01b8c22057b
Kuba <kupca@seznam.cz>
2022-02-13 12:50:37 +0100

    Create new method "getAllNews(Model model)" in NewsController.

--- src/main/java/com/agentesit/academy/backend/admin/news/NewsController.java
+++ src/main/java/com/agentesit/academy/backend/admin/news/NewsController.java
@@ -1,10 +1,32 @@
 package com.agentesit.academy.backend.admin.news;
 
+
+import com.agentesit.academy.backend.service.NewsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+/**
+ * Controller class for News.
+ */
 @Controller
 @RequestMapping("/admin/news")
 public class NewsController {
+
+    /** Instance of NewsService class */
+    @Autowired
+    private NewsService newsService;
+
+    /**
+     * Get all news from database and transmit it to Model.
+     * @return ThymeLeaf template "list.html" with transmitted news in Model.
+     */
+    @GetMapping
+    public String getAllNews(Model model){
+        model.addAttribute("listOfNews", newsService.getAllNews());
+        return "list";
+    }
 
 }
