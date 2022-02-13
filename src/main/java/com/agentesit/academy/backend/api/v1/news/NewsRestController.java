f4a5a1ff9ea352e81df5735dd8964c8259cc1395
Kuba <kupca@seznam.cz>
2022-02-13 11:37:14 +0100

    Add new class NewsRestController.

---
+++ src/main/java/com/agentesit/academy/backend/api/v1/news/NewsRestController.java
@@ -0,0 +1,14 @@
+package com.agentesit.academy.backend.api.v1.news;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Rest Controller class for News.
+ */
+@RestController
+@RequestMapping("/api/v1/news")
+public class NewsRestController {
+
+
+}
