980fae631347ff15e045a4de708111d20ac37871
Kuba <kupca@seznam.cz>
2022-02-13 18:34:02 +0100

    Add another initializing data for database in "BackendApplication" class.

--- src/main/java/com/agentesit/academy/backend/BackendApplication.java
+++ src/main/java/com/agentesit/academy/backend/BackendApplication.java
@@ -3,7 +3,6 @@ package com.agentesit.academy.backend;
 import com.agentesit.academy.backend.domain.CategoryOfNews;
 import com.agentesit.academy.backend.domain.ImageEntity;
 import com.agentesit.academy.backend.domain.NewsEntity;
-import com.agentesit.academy.backend.repository.NewsRepository;
 import com.agentesit.academy.backend.service.NewsService;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
@@ -18,9 +17,10 @@ public class BackendApplication {
 	}
 
 	@Bean
-	public ApplicationRunner initializationDB(NewsRepository newsRepository, NewsService newsService){
+	public ApplicationRunner initializationDB(NewsService newsService){
 		return args -> {
-//			newsRepository.save(new NewsEntity("Nadpis", "Perex", "Obsah", CategoryOfNews.interní));
+			newsService.saveNews(new NewsEntity("Babiše vyhlásila EK jako nejpodstivějšího člověka planety.", "Bohatým bere a chudým dává! Ano uhodli jste správně mluvíme o božím poslu Andreji Babišovi.", "Kecy, kecy a zase jenom kecy.", CategoryOfNews.interní, new ImageEntity("cestaKobrazku", "MIME typ")));
+			newsService.saveNews(new NewsEntity("Preziden Zeman sjel Niagárské vodopády na svém Challengeru 300!", "Neuvěřitelný kousek se povedl našemu prezidentu Miloši Zemanovi - podařilo se mu sjet Niagarské vodopády. Na tento husarský kousek trénoval celé letní prázdniny na svém oblíbeném rybníku na Vysočině.", "Kecy, kecy a zase jenom kecy.", CategoryOfNews.interní, new ImageEntity("cestaKobrazku", "MIME typ")));
 			newsService.saveNews(new NewsEntity("Nadpis", "Perex", "Obsah", CategoryOfNews.interní, new ImageEntity("cestaKobrazku", "MIME typ")));
 		};
 	}
