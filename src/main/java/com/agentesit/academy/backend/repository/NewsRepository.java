3af9016fa63af6d69c40b18a6dbf9f3b8d1b12b9
Kuba <kupca@seznam.cz>
2022-02-21 15:00:56 +0100

    Add filtering method to NewsRepository.

--- src/main/java/com/agentesit/academy/backend/repository/NewsRepository.java
+++ src/main/java/com/agentesit/academy/backend/repository/NewsRepository.java
@@ -11,8 +11,12 @@ import java.util.List;
 /** Repository interface of news */
 public interface NewsRepository extends JpaRepository<NewsEntity, Long> {
 
-    @Query(value = "select * from news n where " +
-            ":#{#newsFilter.getCategoryOfNews()} is null OR n.category = :#{#newsFilter.getCategoryOfNews().name()}",nativeQuery = true)
+//    @Query(value = "select * from news n where " +
+//            ":#{#newsFilter.getCategoryOfNews().name()} is null OR n.category = :#{#newsFilter.getCategoryOfNews().name()}",nativeQuery = true)
+//    List<NewsEntity> getFiltredNews(@Param("newsFilter") NewsFilter newsFilter);
+
+    @Query(value = "select n from news n where " +
+            ":#{#newsFilter.getCategoryOfNews()} is null OR n.category = :#{#newsFilter.getCategoryOfNews()}")
     List<NewsEntity> getFiltredNews(@Param("newsFilter") NewsFilter newsFilter);
 
 }
