cd75823854d8ea5cda8824114eb0c9496261c27d
Kuba <kupca@seznam.cz>
2022-02-26 15:15:52 +0100

    Add saveNews() method with settings image parametters.

--- src/main/java/com/agentesit/academy/backend/service/NewsService.java
+++ src/main/java/com/agentesit/academy/backend/service/NewsService.java
@@ -2,10 +2,18 @@ package com.agentesit.academy.backend.service;
 
 import com.agentesit.academy.backend.domain.NewsEntity;
 import com.agentesit.academy.backend.model.NewsFilter;
+import com.agentesit.academy.backend.model.NewsLogics;
 import com.agentesit.academy.backend.repository.NewsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.List;
 
 /** Class managing data (News) between Controller and Repository. */
@@ -15,12 +23,37 @@ public class NewsService {
 @Autowired
 private NewsRepository newsRepository;
 
+private NewsLogics newsLogics = new NewsLogics();
+
     /**
      * Save (INSERT) new news to database.
      * @param news News to save
      * @return Return the news that was saved.
      */
-    public NewsEntity saveNews(NewsEntity news){
+    public NewsEntity saveNews(NewsEntity news) {
+        return newsRepository.save(news);
+    }
+
+//    public NewsEntity saveNews(NewsEntity news, MultipartFile file){
+//        final String UPLOAD_DIR = "./src/main/resources/uploadsImages/";
+//        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+//
+//        // save the file on the local file system
+//        try {
+//            Path path = Paths.get(UPLOAD_DIR + fileName);
+//            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//    return newsRepository.save(news);
+//}
+public NewsEntity saveNews(NewsEntity news, MultipartFile file){
+    final String UPLOAD_DIR = "./src/main/resources/uploadsImages/";
+
+    Path path = newsLogics.saveFile(UPLOAD_DIR, file);
+
+    news.getImage().setPathToImage(path.toString());
+    news.getImage().setMimeType(newsLogics.checkMIMEtype(path));
     return newsRepository.save(news);
 }
 
