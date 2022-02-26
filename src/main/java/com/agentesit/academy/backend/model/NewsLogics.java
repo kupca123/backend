5b8610cceda1b0ca404ee812ac1e838bff485172
Kuba <kupca@seznam.cz>
2022-02-26 15:15:04 +0100

    Create NewsLogics class.

---
+++ src/main/java/com/agentesit/academy/backend/model/NewsLogics.java
@@ -0,0 +1,45 @@
+package com.agentesit.academy.backend.model;
+
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class NewsLogics {
+
+    public Path saveFile(String uploadDir, MultipartFile file){
+        final String UPLOAD_DIR = uploadDir;
+        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+        Path path = null;
+
+        // save the file on the local file system
+        try {
+            path = Paths.get(UPLOAD_DIR + fileName);
+            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+        } catch (
+                IOException e) {
+            e.printStackTrace();
+        }
+
+        return path;
+    }
+
+    public String checkMIMEtype(Path pathOfFile) {
+
+        String mimeType = "";
+
+        try {
+           mimeType = Files.probeContentType(pathOfFile);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        return mimeType;
+    }
+
+
+}
