d83241d983fdcba8b36a81afd6e07c564e1d0964
Kuba <kupca@seznam.cz>
2022-02-26 17:01:00 +0100

    Reduce commented code in NewsController class

--- src/main/java/com/agentesit/academy/backend/admin/news/NewsController.java
+++ src/main/java/com/agentesit/academy/backend/admin/news/NewsController.java
@@ -1,6 +1,5 @@
 package com.agentesit.academy.backend.admin.news;
 
-
 import com.agentesit.academy.backend.domain.CategoryOfNews;
 import com.agentesit.academy.backend.domain.ImageEntity;
 import com.agentesit.academy.backend.domain.NewsEntity;
@@ -9,16 +8,10 @@ import com.agentesit.academy.backend.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
 /**
  * Controller class for News.
@@ -32,16 +25,6 @@ public class NewsController {
     @Autowired
     private NewsService newsService;
 
-    /**
-     * Get all news from database and transmit it to Model.
-     * @return ThymeLeaf template "list.html" with transmitted news in Model.
-     */
-//    @GetMapping()
-//    public String getAllNews(Model model){
-//        model.addAttribute("listOfNews", newsService.getAllNews());
-//        model.addAttribute("categoryOfNews", CategoryOfNews.values());
-//        return "admin/news/list";
-//    }
 
     /**
      * Gets news from the database by filter. If no filter is set, it will return all news
@@ -64,11 +47,6 @@ public class NewsController {
      * Route to blank create.html form.
      * @return create.html
      */
-//    @GetMapping("/createNews")
-//    public String getCreateForm(){
-//        return "admin/news/create";
-//    }
-
     @GetMapping("/createNews")
     public String getCreateForm(@SessionAttribute("newsFilter") NewsFilter newsFilter, Model model){
         model.addAttribute("filter", newsFilter);
@@ -79,16 +57,11 @@ public class NewsController {
         return "admin/news/create";
     }
 
-//    @PostMapping("/createNews")
-//    public String postCreateNews(@ModelAttribute NewsEntity news, @ModelAttribute ImageEntity image) {
-//        news.setImage(image);
-//        newsService.saveNews(news);
-//        return "redirect:/admin/news";
-//    }
 
-//    @PostMapping("/createNews")
+
+// @PostMapping("/createNews")
 //    public String postCreateNews(@ModelAttribute NewsEntity news, @ModelAttribute ImageEntity image, @RequestParam("imageInput") MultipartFile file, RedirectAttributes attributes) {
-//        final String UPLOAD_DIR = "./src/main/resources/uploadsImages/";
+////        final String UPLOAD_DIR = "./src/main/resources/uploadsImages/";
 //
 //        // check if file is empty
 //        if (file.isEmpty()) {
@@ -96,23 +69,14 @@ public class NewsController {
 //            return "redirect:/admin/news/createNews";
 //        }
 //
-//        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-//
-//        // save the file on the local file system
-//        try {
-//            Path path = Paths.get(UPLOAD_DIR + fileName);
-//            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
 //
 //        news.setImage(image);
-//        newsService.saveNews(news);
+//        newsService.saveNews(news, file);
 //        return "redirect:/admin/news";
 //    }
 
- @PostMapping("/createNews")
-    public String postCreateNews(@ModelAttribute NewsEntity news, @ModelAttribute ImageEntity image, @RequestParam("imageInput") MultipartFile file, RedirectAttributes attributes) {
+@PostMapping("/createNews")
+    public String postCreateNews(@ModelAttribute NewsEntity news, @RequestParam("imageInput") MultipartFile file, RedirectAttributes attributes) {
 //        final String UPLOAD_DIR = "./src/main/resources/uploadsImages/";
 
         // check if file is empty
@@ -121,17 +85,8 @@ public class NewsController {
             return "redirect:/admin/news/createNews";
         }
 
-//        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
-//
-//        // save the file on the local file system
-//        try {
-//            Path path = Paths.get(UPLOAD_DIR + fileName);
-//            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
 
-        news.setImage(image);
+//        news.setImage(image);
         newsService.saveNews(news, file);
         return "redirect:/admin/news";
     }
