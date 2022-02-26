ac528f166151b0716e69498ef42cf872f47c1691
Kuba <kupca@seznam.cz>
2022-02-26 15:13:49 +0100

    Add update postCreateNews() method set image in NewsController class.

--- src/main/java/com/agentesit/academy/backend/admin/news/NewsController.java
+++ src/main/java/com/agentesit/academy/backend/admin/news/NewsController.java
@@ -9,7 +9,16 @@ import com.agentesit.academy.backend.service.NewsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 
 /**
  * Controller class for News.
@@ -70,11 +79,60 @@ public class NewsController {
         return "admin/news/create";
     }
 
-    @PostMapping("/createNews")
-    public String postCreateNews(@ModelAttribute NewsEntity news, @ModelAttribute ImageEntity image) {
+//    @PostMapping("/createNews")
+//    public String postCreateNews(@ModelAttribute NewsEntity news, @ModelAttribute ImageEntity image) {
+//        news.setImage(image);
+//        newsService.saveNews(news);
+//        return "redirect:/admin/news";
+//    }
+
+//    @PostMapping("/createNews")
+//    public String postCreateNews(@ModelAttribute NewsEntity news, @ModelAttribute ImageEntity image, @RequestParam("imageInput") MultipartFile file, RedirectAttributes attributes) {
+//        final String UPLOAD_DIR = "./src/main/resources/uploadsImages/";
+//
+//        // check if file is empty
+//        if (file.isEmpty()) {
+//            attributes.addFlashAttribute("message", "Please select a file to upload.");
+//            return "redirect:/admin/news/createNews";
+//        }
+//
+//        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+//
+//        // save the file on the local file system
+//        try {
+//            Path path = Paths.get(UPLOAD_DIR + fileName);
+//            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        news.setImage(image);
+//        newsService.saveNews(news);
+//        return "redirect:/admin/news";
+//    }
+
+ @PostMapping("/createNews")
+    public String postCreateNews(@ModelAttribute NewsEntity news, @ModelAttribute ImageEntity image, @RequestParam("imageInput") MultipartFile file, RedirectAttributes attributes) {
+//        final String UPLOAD_DIR = "./src/main/resources/uploadsImages/";
+
+        // check if file is empty
+        if (file.isEmpty()) {
+            attributes.addFlashAttribute("message", "Please select a file to upload.");
+            return "redirect:/admin/news/createNews";
+        }
+
+//        String fileName = StringUtils.cleanPath(file.getOriginalFilename());
+//
+//        // save the file on the local file system
+//        try {
+//            Path path = Paths.get(UPLOAD_DIR + fileName);
+//            Files.copy(file.getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
         news.setImage(image);
-        newsService.saveNews(news);
-//        return "admin/news/list";
+        newsService.saveNews(news, file);
         return "redirect:/admin/news";
     }
 
