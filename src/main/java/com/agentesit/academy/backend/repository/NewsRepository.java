815c31929c4f330c1b035def81b5912ddde242a0
Kuba <kupca@seznam.cz>
2022-02-28 01:12:51 +0100

    Add findById() method to NewsRepository interface

--- src/main/java/com/agentesit/academy/backend/repository/NewsRepository.java
+++ src/main/java/com/agentesit/academy/backend/repository/NewsRepository.java
@@ -7,16 +7,17 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 /** Repository interface of news */
 public interface NewsRepository extends JpaRepository<NewsEntity, Long> {
 
-//    @Query(value = "select * from news n where " +
-//            ":#{#newsFilter.getCategoryOfNews().name()} is null OR n.category = :#{#newsFilter.getCategoryOfNews().name()}",nativeQuery = true)
-//    List<NewsEntity> getFiltredNews(@Param("newsFilter") NewsFilter newsFilter);
 
     @Query(value = "select n from news n where " +
             ":#{#newsFilter.getCategoryOfNews()} is null OR n.category = :#{#newsFilter.getCategoryOfNews()}")
     List<NewsEntity> getFiltredNews(@Param("newsFilter") NewsFilter newsFilter);
+
+
+    Optional<NewsEntity> findById(Long id);
 
 }
